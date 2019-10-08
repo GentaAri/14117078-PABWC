f7dca6b22f92ba39c5f163917617cf8063f1281d
GentaAri <49480826+GentaAri@users.noreply.github.com>
2019-10-08 15:33:48 +0700

    Minggu-6

---
+++ 1_1.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Contoh Skrip PHP</title>
+    </head>
+    <body>
+        <?php 
+        $nama = "Genta Ari Anggoro";
+        echo "$nama berkata \'save me save me\' ";
+        ?>
+    </body>
+</html>
---
+++ 1_3.php
@@ -0,0 +1,10 @@
+<?php 
+$a = 5;
+$b = 7;
+echo "a = $a <br>";
+echo "b = $b <br>";
+if ($a == $b)
+    echo "$a sama dengan $b";
+elseif ($a >$b)
+    echo "$a lebih kecil dari pada $b";
+?>
